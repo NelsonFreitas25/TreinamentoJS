7da452e58c50c5e83337942394b779ae19cd5604
NelsonFreitas25 <127761444+NelsonFreitas25@users.noreply.github.com>
2023-09-14 11:34:59 -0300

    exemplos da aula1

---
+++ type-strings.js
@@ -0,0 +1,11 @@
+const texto1 ="Olá mundo!";
+const texto2 ='Olá mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "321456";
+
+    //concatenação (+)
+const citacao = "Meu nome é ";
+const meuNome = "nelson";
+console.log(citacao + meuNome)
+
+//template string ou template literal
