ace6ce8d6d15c437aa70381b4cb355da245c4a1a
NelsonFreitas25 <127761444+NelsonFreitas25@users.noreply.github.com>
2023-09-19 14:07:46 -0300

    operadores e funções

--- parametros.js
+++ parametros.js
@@ -4,10 +4,23 @@ function soma(numero1, numero2) {
    return numero1 + numero2; 
 }
 
- console.log (soma(-454, 2))
- console.log (soma(23, 2))
- console.log (soma(262, 2))
+ //console.log (soma(-454, 2))
+ //console.log (soma(23, 2))
+ //console.log (soma(262, 2))
 
  // parâmetros x argumentos
 
  //ordem dos parâmetros
+
+ function nomeIdade(nome, idade) {
+   return `meu nome é ${nome} e minha idade é ${idade}`;
+   
+ }
+ // console.log(nomeIdade("nelson" , 26))
+
+ function multiplica(numero1, numero2) {
+   return numero1 * numero2;
+
+ }
+                  //9         //6
+console.log(multiplica(soma(4, 5 ), soma (3, 3)))
